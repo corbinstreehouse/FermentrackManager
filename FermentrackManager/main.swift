297b01ab6e7656458509e2bca31990dd1b6e7034
corbin dunn <corbin@corbinstreehouse.com>
2019-10-26 14:41:54 -0700

    Initial Commit

---
+++ FermentrackManager/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FermentrackManager
+//
+//  Created by Corbin Dunn on 10/26/19.
+//  Copyright © 2019 Corbin Dunn. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
